b1803fd5375e45dd662a8fb75834570bb24f0fcd
zhangsan <zhangsan@itcast.cn>
2018-10-29 20:03:52 +0800

    传本地工程文件测试

---
+++ testmygit2/src/myfirstgit.java
@@ -0,0 +1,10 @@
+public class myfirstgit {
+    public static void main(String[] args) {
+        System.out.println("asdasdasd");
+        System.out.println("asdasdasd");
+        System.out.println("asdasdasd");
+        System.out.println("asdasdasd");
+        System.out.println("asdasdasd");
+        System.out.println("asdasdasd");
+    }
+}
